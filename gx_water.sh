91f66b2b9c188edfda8b8466bdae8c894d67f941
Djbrown <offshoresboss@gmail.com>
2023-07-13 18:18:21 +0200

    fix copy water

--- gx_water.sh
+++ gx_water.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker cp gx-event/water/config.yml gx_water:/server/config.yml
+cd ~/gx-event
+clear && docker-compose -f docker-compose.water.yml up -d
+delay 30
+docker cp water/config.yml gx_water:/server/config.yml
