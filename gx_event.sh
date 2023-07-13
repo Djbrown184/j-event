87e13a2ff4f20902e13acac02df4704250b1fd14
Djbrown <offshoresboss@gmail.com>
2023-07-13 18:03:55 +0200

    fix permission

--- gx_event.sh
+++ gx_event.sh
@@ -1,13 +1,13 @@
-chmod 0755 gx-event.sh \
-chmod 0755 gx_water.sh \
-chmod 0755 gx_lobby.sh \
-chmod 0755 gx_team_01.sh \
-chmod 0755 gx_team_02.sh \
-chmod 0755 gx_team_03.sh \
-chmod 0755 gx_team_04.sh \
-chmod 0755 gx_team_05.sh \
-chmod 0755 gx_team_06.sh \
-chmod 0755 gx_team_07.sh \
-chmod 0755 gx_team_08.sh \
-chmod 0755 gx_team_09.sh \
-chmod 0755 gx_team_10.sh
+#!/bin/sh
+docker cp gx-event/water/config.yml gx_water:/server/config.yml
+docker cp gx-event/spigot/spigot.yml gx_lobby:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_01:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_02:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_03:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_04:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_05:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_06:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_07:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_08:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_09:/data/spigot.yml
+docker cp gx-event/spigot/spigot.yml gx_team_10:/data/spigot.yml
