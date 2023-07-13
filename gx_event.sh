0ba9a0c45f6c6b19acea4e3f8f57cb5c69a1b9ee
Djbrown <offshoresboss@gmail.com>
2023-07-13 17:51:43 +0200

    fix permission

---
+++ gx_event.sh
@@ -0,0 +1,13 @@
+chmod 0755 gx-event.sh \
+chmod 0755 gx_water.sh \
+chmod 0755 gx_lobby.sh \
+chmod 0755 gx_team_01.sh \
+chmod 0755 gx_team_02.sh \
+chmod 0755 gx_team_03.sh \
+chmod 0755 gx_team_04.sh \
+chmod 0755 gx_team_05.sh \
+chmod 0755 gx_team_06.sh \
+chmod 0755 gx_team_07.sh \
+chmod 0755 gx_team_08.sh \
+chmod 0755 gx_team_09.sh \
+chmod 0755 gx_team_10.sh
